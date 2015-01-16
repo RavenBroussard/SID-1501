6eb7f3473407c06d9f4912fd8bc6210d0b7e3c4c
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2015-01-15 22:29:27 -0600

    getting there

--- sdi-project2 2015/Broussard_Raven_Assignment2.js
+++ sdi-project2 2015/Broussard_Raven_Assignment2.js
@@ -6,12 +6,56 @@
 
 // Variables
 
-var  = ,
+var caylaMood = false;
+var familyMembers = ["Nikki", "Cayla", "Cyrus"];
+var timeForBus = 0645;
+var positiveMood = "I want you to know that you are \"Awesome\" just by being yourself!";
+var selfEsteem = "Tell me something that makes you special?"
+var caylaWorkout = 2;
+
+var morningRoutine = function (alarmClock){
+    if (alarmClock === true)
+    {
+        console.log("Stupid clock never let's me sleep, I love you, but I hate you!");
+    }
+    else
+    {
+        console.log("Hmm, I wonder why I can't sleep.");
+    }
+}
 
 
+var letsGetGoing = function (moveit){
+    var late;
+    var timeToMakeItHappen = 0700;
+
+    if (timeToMakeItHappen > late){
+        late = true;
+    }
+    else{
+        late = false;
+    }
+    return late;
 
 
+}
 
-;
+var cayla = function(therapy){
+    var feelBetter;
+    var caylaWorkout = 2;
+    while (minutes >= feelBetter) {
+        console.log("Cayla needs " + caylaWorkout + " minutes to feel better.");
+        caylaWorkout = caylaWorkout + 2;
+        feelBetter = (caylaWorkout / 2) - 1;
 
+    }
+    return feelBetter;
+}
+
+var goodMornings = function(first, second)
+{
+    var middle = "You're so special ";
+    var conversation = first + middle + second;
+    return conversation;
+}
 
