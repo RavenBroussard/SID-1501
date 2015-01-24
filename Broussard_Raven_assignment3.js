475af953447acd0aebe9811cc383112954891fe7
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2015-01-23 20:17:04 -0600

    getting started

---
+++ Broussard_Raven_assignment3.js
@@ -0,0 +1,28 @@
+/*  Raven "Nikki" Broussard
+    Project 3
+    SDI 1501 Week 3
+    January 22, 2015
+ */
+
+
+// Variables
+
+var myName = "Nikki";
+var timeOutDoor = 45;
+var itemsNeeded = ["2 book bags", "1 purse", "1 laptop", "2 lunches"];
+var ourStuff = {
+    "school stuff": "Book bags",
+    "my stuff": "Purse",
+    "equipment": "Laptop",
+    "food": "Lunches"};
+
+
+// Json
+console.log();
+var handleData = function(json){
+    for (var i = 0; i < jason.haul.length;i++){
+        var haul = json.haul[i];
+        console.log();
+    };
+};
+
---
+++ Json.js
@@ -0,0 +1,24 @@
+var json = {
+    "haul" : [
+        {   "item": "School Stuff",
+            "type": "Book bag",
+            "quantity": 2
+
+        },
+        {   "item": "My Stuff",
+            "type": "Purse",
+            "quantity": 1
+
+        },
+        {   "item": "Equipment",
+            "type": "Laptop",
+            "quantity": 1
+
+        },
+        {   "item": "Food",
+            "type": "Lunches",
+            "quantity": 2
+
+        },
+    ]
+}
